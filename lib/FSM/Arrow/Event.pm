08f1157e51a5bc019cdbe78b2afcd178888bda6d
Konstantin S. Uvarin <khedin@gmail.com>
2015-10-04 18:34:39 +0300

    API Add FSM::Arrow::Event->generator_regex for making on_check_event callbacks

--- lib/FSM/Arrow/Event.pm
+++ lib/FSM/Arrow/Event.pm
@@ -9,7 +9,10 @@ FSM::Arrow::Event - event base class for FSM::Arrow state machine.
 
 =cut
 
-our $VERSION = 0.05;
+use Carp;
+our @CARP_NOT = qw(FSM::Arrow FSM::Arrow::Instance);
+
+our $VERSION = 0.0501;
 
 =head1 METHODS
 
@@ -33,6 +36,7 @@ This value will be used to trigger SM transactions.
 
 sub new {
 	my ($self, %opt) = @_;
+
 	return bless \%opt, $self;
 };
 
@@ -54,6 +58,66 @@ Returns event's raw data (if specified).
 
 sub raw {
 	return $_[0]->{raw};
+};
+
+=head1 GENERATOR METHODS
+
+These static methods create functions
+that can make FSM::Arrow::Event objects from incoming raw data.
+This may be useful in conjunction with L< FSM::Arrow>'s C<on_check_event>
+callback.
+
+=head3 generator_regex ( %args )
+
+Static method, returns a coderef that constructs
+FSM::Arrow::Event from given string.
+
+%args may include:
+
+=over
+
+=item * regex (required) - the regular expression used to parse string.
+
+Any named capture groups, including C<(?\<type\>...)>,
+would become event's fields.
+
+First capture group would become type if no type group is defined.
+
+=item * stop - event type that is returned for undefined string.
+Default is C<__STOP__>.
+
+=item * unknown - event type that is returned for no type.
+Default is C<__ANY__>.
+
+=back
+
+=cut
+
+sub generator_regex {
+	my ($class, %args) = @_;
+
+	defined $args{regex} or croak __PACKAGE__
+		."->generator_regex: regex parameter must be present";
+
+	my $re = $args{regex};
+	$re = qr($re) unless ref $re eq 'Regexp';
+
+	my $stop    = defined $args{stop}    ? $args{stop}    : "__STOP__";
+	my $unknown = defined $args{unknown} ? $args{unknown} : "__ANY__";
+
+	return sub {
+		return $class->new(type => $stop, raw => undef)
+			unless defined $_[0];
+
+		$_[0] =~ $re or croak "Raw event doesn't match regex $re";
+
+		no warnings 'uninitialized'; ## no critic
+		return $class->new (
+			raw => $_[0],
+			type => length $1 ? $1 : $unknown,
+			%+,
+		);
+	};
 };
 
 1;
---
+++ t/24-event-gen.t
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+
+use FSM::Arrow::Event;
+
+
+my $ev;
+my $gen;
+
+note "Rex generator 1";
+$gen = FSM::Arrow::Event->generator_regex( regex => qr/(\w+)/ );
+
+$ev = $gen->( "foo bar" );
+is (ref $ev, "FSM::Arrow::Event", "Ev ref holds");
+is ($ev->type, "foo", "Ev type holds");
+is ($ev->raw, "foo bar", "Ev raw holds");
+
+$ev = $gen->();
+is ($ev->type, "__STOP__", "STOP ev type holds");
+
+eval { $gen->("???"); };
+like ($@, qr(event.*match), "Bad events shall not pass");
+
+if ($] > 5.010) {
+	note "Rex generator 2 - with named capture";
+	$gen = FSM::Arrow::Event->generator_regex(
+		regex => q/((\d+\.)\s+)?(?<type>[a-z]\w*)(\s+(?<number>\d+))?/,
+	);
+
+	$ev = $gen->("1. aaa 42");
+	is ($ev->type, "aaa", "type holds");
+	is ($ev->{number}, 42, "other capture holds");
+};
+
+done_testing;
